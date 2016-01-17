9351a944a83d4c2b80a80d2773e95aa415a5092d
Tom <tom@hobofife.com>
2016-01-17 07:12:47 -0600

    Highlight line based on color scheme

--- appearance.el
+++ appearance.el
@@ -15,3 +15,26 @@
 
 ;; thinner border -> │
 ;; thicker border -> ┃
+
+
+;; http://emacs.stackexchange.com/questions/9740/how-to-define-a-good-highlight-face
+;; http://stackoverflow.com/questions/17701576/changing-highlight-line-color-in-emacs
+(require'color)
+
+(defun set-hl-line-color-based-on-theme ()
+  "Sets the hl-line face to have no foregorund and a background
+    that is 10% darker than the default face's background."
+  (set-face-attribute 'hl-line nil
+                      :underline nil
+                      :foreground nil
+                      :background (color-darken-name (face-background 'default) 3))
+  )
+
+(defun keep-syntax-highlighting ()
+  (interactive)
+  (set-face-foreground 'highlight nil))
+
+(add-hook 'hl-line-mode-hook 'set-hl-line-color-based-on-theme)
+(add-hook 'hl-line-mode-hook 'keep-syntax-highlighting)
+
+(provide 'appearance)
