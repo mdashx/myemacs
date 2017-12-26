f150b6d9f0b11bb2937cce0ef4480fc79489cc13
tom <tom@mdashx.com>
2017-12-26 09:36:04 -0600

    ROT13 command

--- init.el
+++ init.el
@@ -6,8 +6,8 @@
 ;;       (tool-bar-mode -1)
 ;;       (scroll-bar-mode -1)))
 
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
+(if (functionp 'tool-bar-mode) (tool-bar-mode -1))
+(if (functionp 'scroll-bar-mode) (scroll-bar-mode -1))
 (menu-bar-mode -1)
 
 ;; Load Path
@@ -79,6 +79,16 @@
 
 ;; (require 'ignore-modification-time)
 
+(global-set-key (kbd "C-x g") 'magit-status)
+
+;; The command to toggle ROT13 doesn't take effect until after I type
+;; any other key after the key sequence. I'll figure that out... I'm
+;; going to be adding my own ROT47 function anyway.
+(define-prefix-command 'toms-map)
+(define-key toms-map (kbd "e") 'toggle-rot13-mode)
+(global-set-key (kbd "C-q") toms-map)
+
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -91,10 +101,11 @@
  '(fci-rule-color "#3E4451")
  '(package-selected-packages
    (quote
-    (flow-minor-mode flycheck flycheck-flow prettier-js lua-mode go-guru go-mode atom-dark-theme atom-one-dark-theme traad markdown-mode nginx-mode yaml-mode web-mode company helm helm-core powerline color-theme))))
+    (minesweeper git-gutter magit flow-minor-mode flycheck flycheck-flow prettier-js lua-mode go-guru go-mode atom-dark-theme atom-one-dark-theme traad markdown-mode nginx-mode yaml-mode web-mode company helm helm-core powerline color-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
