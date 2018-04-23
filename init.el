3f16eaee058eef7000528b097e9df9b06f58047c
tom <tom@mdashx.com>
2018-04-23 16:49:25 +0000

    Change colors for company-mode overlay (when Ivy isn't working, i.e. with Elpy)

--- init.el
+++ init.el
@@ -113,6 +113,20 @@
 (define-key company-active-map (kbd "C-n") 'company-select-next-or-abort)
 (define-key company-active-map (kbd "C-p") 'company-select-previous-or-abort)
 
+
+;; https://www.emacswiki.org/emacs/CompanyMode#toc7
+;; Change the company mode colors
+(require 'color)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-scrollbar-bg ((t (:background ,(color-lighten-name bg 10)))))
+   `(company-scrollbar-fg ((t (:background ,(color-lighten-name bg 5)))))
+   `(company-tooltip-selection ((t (:inherit font-lock-function-name-face))))
+   `(company-tooltip-common ((t (:inherit font-lock-constant-face))))))
+
+
 ;; https://github.com/jorgenschaefer/elpy/issues/1084
 ;; Elpy doesn't work with Ivy
 (elpy-enable)
