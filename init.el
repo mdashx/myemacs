d7c092c724ef7a3621b789eb8692bc58aa9ebbd1
tom <tom@mdashx.com>
2021-12-29 13:31:48 -0600

    packages, completion, keybindings, nav, etc.

--- init.el
+++ init.el
@@ -1,20 +1,19 @@
 (require 'package)
 (require 'cl-lib)
 
-;; Startup
-(setq inhibit-startup-message t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Packages
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defvar my-packages
   '(
+    avy
+    company
+    counsel ;; this also installs ivy and swiper as dependencies
+    hydra
     powerline
-    ;; company
-    ;; counsel
-    ;; ivy
-    ;; swiper
-    ;; helm-core
-    ;; helm
-
+    which-key
     )
   )
 
@@ -31,12 +30,134 @@
   ;; install the missing packages
   (dolist (p my-packages)
     (when (not (package-installed-p p))
-            (package-install p))))
+      (package-install p))))
 
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Aesthetics
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq inhibit-startup-message t)
 (if (functionp 'tool-bar-mode) (tool-bar-mode -1))
 (if (functionp 'scroll-bar-mode) (scroll-bar-mode -1))
 (menu-bar-mode -1)
 
 (load-theme 'wombat t)
 (powerline-default-theme)
+(set-face-attribute 'default nil :font "Inconsolata-16")
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Usability
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(global-display-line-numbers-mode 1)
+(global-hl-line-mode 1)
+
+(defun set-hl-line-color-based-on-theme ()
+  "Sets the hl-line face to have no foregorund and a background
+    that is 2% darker than the default face's background."
+  (set-face-attribute 'hl-line nil
+                      :underline nil
+                      :foreground nil
+                      :background (color-darken-name
+                                   (face-background 'default) 2)))
+
+
+(defun keep-syntax-highlighting ()
+  (interactive)
+  (set-face-foreground 'highlight nil))
+
+(add-hook 'global-hl-line-mode-hook 'set-hl-line-color-based-on-theme)
+(add-hook 'global-hl-line-mode-hook 'keep-syntax-highlighting)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Programming modes
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(add-hook 'prog-mode-hook 'hs-minor-mode)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Keybindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; My keybindings
+(defhydra hydra-folding ()
+  "
+Hide^^            ^Show^            ^Toggle^    ^Navigation^
+----------------------------------------------------------------
+_h_ hide all      _s_ show all      _t_oggle    _n_ext line
+_d_ hide block    _a_ show block              _p_revious line
+_l_ hide level
+
+_SPC_ cancel
+"
+   ("s" hs-show-all)
+   ("h" hs-hide-all)
+   ("a" hs-show-block)
+   ("d" hs-hide-block)
+   ("t" hs-toggle-hiding)
+   ("l" hs-hide-level)
+   ("n" forward-line)
+   ("p" (forward-line -1))
+   ("SPC" nil)
+)
+
+(defhydra hydra-show-lines ()
+   ("s" (global-display-line-numbers-mode 1))
+   ("h" (global-display-line-numbers-mode -1))
+   ("a" (global-hl-line-mode 1))
+   ("d" (global-hl-line-mode -1))
+  )
+
+(defhydra hydra-base (global-map "C-j")
+  "
+Options^^
+---------
+_f_ folding  
+_s_ search
+
+_SPC_ cancel
+"
+  ("f" hydra-folding/body :exit t)
+  ("j" avy-goto-char :exit t)
+  ("n" hydra-show-lines/body :exit t)
+  ("s" swiper :exit t)
+  ("SPC" nil)
+  )
+
+(global-display-line-numbers-mode)
+
+
+;; Overriding defaults with my preferred tools
+(global-set-key (kbd "C-s") 'search-forward)
+(global-set-key (kbd "M-x") 'counsel-M-x)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "C-x b") 'counsel-switch-buffer)
+(global-unset-key "\C-z") ;; Because I hate accidentally suspending Emacs
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Completion
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; https://oremacs.com/swiper/#getting-started
+(ivy-mode)
+(setq ivy-use-virtual-buffers t)
+(setq ivy-count-format "(%d/%d) ")
+
+(require 'which-key)
+(which-key-mode)
+
+(require 'company)
+(global-company-mode)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Indentation
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
