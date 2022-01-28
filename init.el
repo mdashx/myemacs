e24914b385d9be7e3cee0cbc8b492845399fb0db
tom <tom@mdashx.com>
2022-01-28 09:31:43 -0600

    stuff

--- init.el
+++ init.el
@@ -1,7 +1,6 @@
 (require 'package)
 (require 'cl-lib)
 
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -9,10 +8,22 @@
 (defvar my-packages
   '(
     avy
+    blacken
     company
     counsel ;; this also installs ivy and swiper as dependencies
+    diff-hl
+    flycheck
     hydra
+    lsp-jedi
+    lsp-mode
+    plantuml-mode
     powerline
+    prettier-js
+    projectile
+    python-isort
+    rainbow-delimiters
+    use-package
+    web-mode
     which-key
     )
   )
@@ -45,14 +56,22 @@
 (load-theme 'wombat t)
 (powerline-default-theme)
 (set-face-attribute 'default nil :font "Inconsolata-16")
+;; (set-face-attribute 'default nil :font "Inconsolata-12")
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Usability
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(global-display-line-numbers-mode 1)
-(global-hl-line-mode 1)
+;; I hate the annoying backup files...
+(setq make-backup-files nil)
+(setq create-lockfiles nil)
+(setq auto-save-default nil)
+
+;; And I hate the annoying bell
+(setq ring-bell-function 'ignore)
+
+(require 'color)
 
 (defun set-hl-line-color-based-on-theme ()
   "Sets the hl-line face to have no foregorund and a background
@@ -71,19 +90,31 @@
 (add-hook 'global-hl-line-mode-hook 'set-hl-line-color-based-on-theme)
 (add-hook 'global-hl-line-mode-hook 'keep-syntax-highlighting)
 
+(global-diff-hl-mode)
+(global-display-line-numbers-mode 1)
+(global-hl-line-mode 1)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Programming modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(require 'lsp-mode)
 (add-hook 'prog-mode-hook 'hs-minor-mode)
-
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(electric-pair-mode)
+(show-paren-mode)
+(projectile-mode 1)
+(global-flycheck-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Keybindings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+
 ;; My keybindings
+
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
 (defhydra hydra-folding ()
   "
 Hide^^            ^Show^            ^Toggle^    ^Navigation^
@@ -130,13 +161,15 @@ _SPC_ cancel
 
 (global-display-line-numbers-mode)
 
-
 ;; Overriding defaults with my preferred tools
 (global-set-key (kbd "C-s") 'search-forward)
 (global-set-key (kbd "M-x") 'counsel-M-x)
 (global-set-key (kbd "C-x C-f") 'counsel-find-file)
 (global-set-key (kbd "C-x b") 'counsel-switch-buffer)
 (global-unset-key "\C-z") ;; Because I hate accidentally suspending Emacs
+
+;; (setq lsp-keymap-prefix "s-l")
+;; (global-set-key (kbd "C-c l") lsp-command-map)
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -160,4 +193,82 @@ _SPC_ cancel
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (setq-default indent-tabs-mode nil)
-(setq-default tab-width 4)
+(setq-default tab-width 2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Python
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; (add-hook 'python-mode-hook 'blacken-mode)
+;; (add-hook 'python-mode-hook 'python-isort-on-save-mode)
+(add-hook 'python-mode-hook 'lsp)
+
+(use-package lsp-jedi
+  :ensure t
+  :config
+  (with-eval-after-load "lsp-mode"
+    (add-to-list 'lsp-disabled-clients 'pyls)
+    (add-to-list 'lsp-enabled-clients 'jedi)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; JavaScript
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.ctp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.vue?\\'" . web-mode))
+
+(defvar mmm-submode-decoration-level 0)
+(defvar js-indent-level 2)
+
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+
+(add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+(add-to-list 'web-mode-indentation-params '("lineup-concats" . t))
+(add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
+
+;; (setq web-mode-script-padding 0)
+;; ;; http://cha1tanya.com/2015/06/20/configuring-web-mode-with-jsx.html
+;; ;; Use JSX content for all JavaScript files
+;; (if (equal web-mode-content-type "javascript")
+;;     (web-mode-set-content-type "jsx"))
+
+;; (setq web-mode-enable-auto-closing t)
+
+(require 'prettier-js)
+
+;; (setq prettier-js-args '(
+;;   "--trailing-comma" "es5"
+;;   "--single-quote" "false"
+;; ))
+
+
+;; Other modes
+(add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
+(add-to-list 'auto-mode-alist '("\\.puml\\'" . plantuml-mode))
+(setq plantuml-default-exec-mode 'executable)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(plantuml-mode prettier-js yasnippet which-key web-mode use-package rainbow-delimiters pyvenv python-isort pyenv-mode projectile powerline lsp-jedi hydra highlight-indentation flycheck diff-hl counsel company-anaconda blacken avy)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(put 'upcase-region 'disabled nil)
